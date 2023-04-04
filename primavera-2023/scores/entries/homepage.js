2a6ea82c6b7e49b2327352819f86c71936fc2905
Astro <astromp777@gmail.com>
2023-04-04 20:16:16 +0000

    4/4/23

---
+++ primavera-2023/scores/entries/homepage.js
@@ -0,0 +1,216 @@
+var entries = [
+  {
+    names: ["Relfex"],
+    link: "https://www.khanacademy.org/computer-programming/virtus-homepage/6210855948828672",
+    team: "Virtus",
+  },
+  {
+    names: ["Dipper98"],
+    link: "https://www.khanacademy.org/computer-programming/aequor-team-homepage/6503300850827264",
+    team: "Aeqour",
+  },
+
+  {
+    names: ["The Dalek Overlord"],
+    link: "https://www.khanacademy.org/computer-programming/team-magus-homepage-entry/4811409071587328",
+    team: "Magus",
+  },
+
+  {
+    names: ["♢ Waves ♢"],
+    link: "https://www.khanacademy.org/computer-programming/aequor-homepage/6299795401785344",
+    team: "Aequor",
+  },
+  {
+    names: ["sugar"],
+    link: "https://khanacademy.org/cs/i/5842144624689152",
+    team: "Virtus",
+  },
+  {
+    names: ["PaNcAkEs"],
+    link: "https://www.khanacademy.org/computer-programming/team-opes-homepage-wip/6178630146506752",
+    team: "Opes",
+  },
+  {
+    names: ["Deathly Hallows"],
+    link: "https://www.khanacademy.org/computer-programming/team-virtus/6075170080210944",
+    team: "Virtus",
+  },
+  {
+    names: ["Shannon"],
+    link: "https://www.khanacademy.org/computer-programming/aqueor-homepage-wip/4652073603088384",
+    team: "Aequor",
+  },
+  {
+    names: [21262],
+    link: "https://www.khanacademy.org/computer-programming/team-magus-primavera-2023/6010614104506368",
+    team: "Magus",
+  },
+  {
+    names: ["JDS"],
+    link: "https://www.khanacademy.org/computer-programming/cinis-homepage/5575429235982336",
+    team: "Cinis",
+  },
+  {
+    names: ["skylands JS"],
+    link: "https://www.khanacademy.org/computer-programming/aequor-homepage/4588882730729472",
+    team: "Aequor",
+  },
+  {
+    names: ["Classic Kid"],
+    link: "https://www.khanacademy.org/computer-programming/team-virtus-homepage/6704094623612928",
+    team: "Virtus",
+  },
+  {
+    names: ["KarLlr"],
+    link: "https://www.khanacademy.org/computer-programming/new-webpage/6465772433227776",
+    team: "Aequor",
+  },
+  {
+    names: ["the Knight_400"],
+    link: "https://www.khanacademy.org/computer-programming/team-magus-homepage/4681815119151104",
+    team: "Magus",
+  },
+  {
+    names: ["LightningCoder"],
+    link: "https://www.khanacademy.org/computer-programming/team-aqeuor-homepage/6036280258641920",
+    team: "Aequor",
+  },
+
+  {
+    names: ["🍕TheGreatPizzaMan🍕"],
+    link: "https://www.khanacademy.org/computer-programming/team-cinis-homepage/6357830207848448",
+    team: "Cinis",
+  },
+
+  {
+    names: ["Jme", "Hannah"],
+    link: "https://www.khanacademy.org/computer-programming/cinis-page/6203597353435136",
+    team: "Cinis",
+  },
+  {
+    names: ["ŁĄŻĒŖŴŌŁF", "StuffStash"],
+    link: "https://www.khanacademy.org/cs/i/5478099195314176",
+    team: "Altum",
+  },
+  {
+    names: ["Yokiebob"],
+    link: "https://www.khanacademy.org/computer-programming/cinis-homepage/4911914829529088",
+    team: "Cinis",
+  },
+  {
+    names: ["Fire Wolf"],
+    link: "https://www.khanacademy.org/computer-programming/altum/6297182643994624",
+    team: "Altum",
+  },
+  {
+    names: ["ChickCoder"],
+    link: "https://www.khanacademy.org/computer-programming/team-virtus/5821851885682688",
+    team: "Virtus",
+  },
+  {
+    names: ["Galaxy Ranger"],
+    link: "https://www.khanacademy.org/computer-programming/my-homepage-for-team-aequor/6372871544029184",
+    team: "Aequor",
+  },
+  {
+    names: ["Jar-Jar Binks"],
+    link: "https://www.khanacademy.org/computer-programming/homepage-ig/6219472240427008",
+    team: "Aequor",
+  },
+  {
+    names: ["Samurai Warrior"],
+    link: "https://www.khanacademy.org/computer-programming/team-aequor-hompage-primavera-2023/5419320655265792",
+    team: "Aequor",
+  },
+  {
+    names: ["Avinasen"],
+    link: "https://www.khanacademy.org/computer-programming/team-cinis-homepage/5673622682517504",
+    team: "Cinis",
+  },
+  {
+    names: ["_"],
+    link: "https://www.khanacademy.org/computer-programming/team-aequor-homepage/5657313961885696",
+    team: "Aequor",
+  },
+  {
+    names: ["RedSus"],
+    link: "https://www.khanacademy.org/cs/i/4988706739240960?width=600",
+    team: "Magus",
+  },
+  {
+    names: ["vw_beetlesultimategreatness"],
+    link: "https://www.khanacademy.org/computer-programming/team-altum-homepage/6493119857344512",
+    team: "Altum",
+  },
+  {
+    names: ["Velvet"],
+    link: "https://www.khanacademy.org/computer-programming/team-altum-homepage/6403515498151936",
+    team: "Altum",
+  },
+  {
+    names: ["The Ember Society"],
+    link: "https://www.khanacademy.org/computer-programming/team-opes-hompage-new-lets-see-aequor-top-this/6692618403889152",
+    team: "Opes",
+  },
+  {
+    names: ["Lemon Games"],
+    link: "https://www.khanacademy.org/computer-programming/team-cinis-primavera-2023/5594171525349376",
+    team: "Cinis",
+  },
+  {
+    names: ["jfsh"],
+    link: "https://www.khanacademy.org/computer-programming/team-magus-homepage-entry-primavera-2023/6412670355161088",
+    team: "Magus",
+  },
+  {
+    names: ["sgh07"],
+    link: "https://www.khanacademy.org/computer-programming/aequor-homepage-primavera-entry/6046973494083584",
+    team: "Aequer",
+  },
+  {
+    names: ["Fireball my dear sir"],
+    link: "https://www.khanacademy.org/computer-programming/team-cinis-homepage-primavera/5213562864975872",
+    team: "Cinis",
+  },
+  {
+    names: ["Snowflake"],
+    link: "https://www.khanacademy.org/computer-programming/i-do-not-exist/5422770663243776",
+    team: "Altum",
+  },
+  {
+    names: ["schuyler"],
+    link: "https://www.khanacademy.org/computer-programming/altum-homepage/4964091778351104",
+    team: "Altum",
+  },
+  {
+    names: ["Masqueet"],
+    link: "https://www.khanacademy.org/computer-programming/opes-homepage/5478559436292096",
+    team: "Opes",
+  },
+  {
+    names: ["Shipment22"],
+    link: "https://www.khanacademy.org/computer-programming/i/6154522050478080",
+    team: "Aequor",
+  },
+  {
+    names: ["Eryn"],
+    link: "https://www.khanacademy.org/computer-programming/team-altum-homepage/6126326479372288",
+    team: "Altum",
+  },
+  {
+    names: ["codeMaster"],
+    link: "https://www.khanacademy.org/computer-programming/team-opes-homepage/4599829218246656",
+    team: "Opes",
+  },
+  {
+    names: ["Falcon Jumper"],
+    link: "https://www.khanacademy.org/computer-programming/smart-page/4579138632728576",
+    team: "Virtus",
+  },
+  {
+    names: ["WinstonWinner000 (Parlor Indie)"],
+    link: "https://www.khanacademy.org/computer-programming/team-opes-homepage/6204874342842368",
+    team: "Opes",
+  },
+];
